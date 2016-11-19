231bf7956a4603bf7c51f5fac80b4bf35810627b
IOriens <djjoriens@gmail.com>
2016-11-19 11:49:09 +0800

    测试新版上传script

--- update.sh
+++ update.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+message="Just added some code..."
+
+
 git add . --all
-git commit -m "Just added some shitty code..."
+
+
+if [ ${#1} -gt 0 ] 
+then 
+    echo $1
+    git commit -m $1
+else 
+    git commit -m $message
+fi
+
 git push -u
 
