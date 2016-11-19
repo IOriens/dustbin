b66514f1c73b9113152a04cb059fd3590a3aed35
IOriens <djjoriens@gmail.com>
2016-11-19 12:09:16 +0800

    Just added some code...

--- update.sh
+++ update.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 message="Just added some code..."
 
-
 git add . --all
-
 
 if [ ${#1} -gt 0 ] 
 then 
     echo $1
     git commit -m "$1"
 else 
-    git commit -m $message
+    git commit -m "$message"
 fi
 
 git push -u
