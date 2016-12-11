f1a74c8ade98271da5fbdadcdcff4e8ddad99b13
IOriens <djjoriens@gmail.com>
2016-12-11 15:33:57 +0800

    add text sorter

---
+++ fe-works/textSorter/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Mluti-Line Text Sorter</h1>
+    <inout></inout>
+  </div>
+</template>
+
+<script>
+import InOut from './components/InOut';
+
+export default {
+  name: 'app',
+  components: {
+    inout: InOut,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: px;*/
+  padding: 5px 40px;
+}
+</style>
---
+++ fe-works/textSorter/src/components/InOut.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="inout">
+    <textarea v-model="txt" @keydown="handleChange()" placeholder="Input some text">
+    </textarea>
+    <input type="checkbox" id="rev" v-model="rev" @change="handleChange()"><label for="rev">reverse</label>
+    <output v-html="sortedText"></output>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      txt: '',
+      sortedText: '',
+      rev: false,
+    };
+  },
+  methods: {
+    handleChange() {
+      let newTxt = this.txt.split('\n').map(t => t.trim());
+      newTxt = this.rev ? newTxt.sort().reverse() : newTxt.sort();
+      newTxt = newTxt.join('<br/>');
+      this.sortedText = newTxt;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  textarea {
+    display: block;
+    width: 100%;
+    height: 200px;
+    resize: none;
+  }
+
+  .inout {
+    text-align: left;
+  }
+
+  output {
+    display: block;
+    margin: 20px auto;
+    text-align: left;
+  }
+</style>
